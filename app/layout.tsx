454fd679be589b061f5b035a54c4e1bf5eb5cb53
smy1999 <865988027@qq.com>
2024-08-17 12:53:51 +0800

    [Feature] Authentication [Comment] Clerk Authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import {ClerkProvider, SignedIn, SignedOut, SignIn} from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <SignedIn>
+            {children}
+          </SignedIn>
+          <SignedOut>
+            <SignIn/>
+          </SignedOut>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
