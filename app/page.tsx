3e5fea32308a80dcc366ddc6f547aa12f6181e38
smy1999 <865988027@qq.com>
2024-08-17 16:39:45 +0800

    [Feature] Filter Option [Comment] Cars filter option.

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@
 
 import Hero from "@/components/Home/Hero";
 import SearchInput from "@/components/Home/SearchInput";
+import CarsFiltersOption from "@/components/Home/CarsFiltersOption";
 
 export default function Home() {
   return (
     <div className={"p-5 sm:px-10 md:px-20"}>
       <Hero/>
       <SearchInput/>
+      <CarsFiltersOption/>
     </div>
   );
 }
---
+++ components/Home/CarsFiltersOption.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const CarsFiltersOption = () => {
+  return (
+    <div className={'mt-10 flex items-center justify-between'}>
+      <div>
+        <h2 className={'text-[30px] font-bold'}>
+          Cars Catalog
+        </h2>
+        <h2>Explore our cars you might likes</h2>
+      </div>
+      <div className={'flex gap-5'}>
+        <select className="select select-bordered w-full max-w-xs ">
+          <option disabled selected>Price</option>
+          <option>Min to Max</option>
+          <option>Max to Min</option>
+        </select>
+        <select className="select select-bordered md:block w-full max-w-xs hidden">
+          <option disabled selected>Manufacture</option>
+          <option>Honda</option>
+          <option>BMW</option>
+          <option>Toyota</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default CarsFiltersOption;
