dbe39766b5023cc815336e98d551a9739f2c4d57
smy1999 <865988027@qq.com>
2024-08-17 17:51:17 +0800

    [Feature] Items [Comment] Get items from backend.

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,22 @@
 import Hero from "@/components/Home/Hero";
 import SearchInput from "@/components/Home/SearchInput";
 import CarsFiltersOption from "@/components/Home/CarsFiltersOption";
+import {useEffect, useState} from "react";
+import {getCarsList} from "@/services";
 
 export default function Home() {
+
+  const [carsList, setCarsList] = useState<any>([])
+
+  useEffect(() => {
+    getCarsList_()
+  }, [])
+
+  const getCarsList_ = async () => {
+    const result:any = await getCarsList();
+    setCarsList(result?.carsList)
+  }
+
   return (
     <div className={"p-5 sm:px-10 md:px-20"}>
       <Hero/>
---
+++ services/index.tsx
@@ -0,0 +1,21 @@
+import request, {gql} from "graphql-request"
+
+export const getCarsList = async () => {
+  const query = gql`
+    query CarLists {
+      carLists {
+        carAvg
+        id
+        name
+        price
+        seat
+        publishedAt
+        updatedAt
+        createdAt
+      }
+    }
+  `
+  const result = await request('https://us-west-2.cdn.hygraph.com/content/clzwvl2pn00c407uq8355dzmi/master', query);
+  return result;
+}
+
